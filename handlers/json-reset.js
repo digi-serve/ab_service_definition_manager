4e01a740d3f2f7aaba579181e88335851b6bbd24
Johnny Hausman <johnny@hausmanfamily.com>
2021-08-23 16:52:22 +0700

    [add] include testing handler for resetting the active ABDefinitions.

---
+++ handlers/json-reset.js
@@ -0,0 +1,55 @@
+/**
+ * json-reset
+ * Signal our running services to clear their ABDefinitions.
+ */
+
+const ABBootstrap = require("../AppBuilder/ABBootstrap");
+// {ABBootstrap}
+// responsible for initializing and returning an {ABFactory} that will work
+// with the current tenant for the incoming request.
+
+module.exports = {
+   /**
+    * Key: the cote message key we respond to.
+    */
+   key: "definition_manager.json-reset",
+
+   /**
+    * inputValidation
+    * define the expected inputs to this service handler:
+    * Format:
+    * "parameterName" : {
+    *    {joi.fn}   : {bool},  // performs: joi.{fn}();
+    *    {joi.fn}   : {
+    *       {joi.fn1} : true,   // performs: joi.{fn}().{fn1}();
+    *       {joi.fn2} : { options } // performs: joi.{fn}().{fn2}({options})
+    *    }
+    *    // examples:
+    *    "required" : {bool},
+    *    "optional" : {bool},
+    *
+    *    // custom:
+    *        "validation" : {fn} a function(value, {allValues hash}) that
+    *                       returns { error:{null || {new Error("Error Message")} }, value: {normalize(value)}}
+    * }
+    */
+   inputValidation: {
+      // json: { object: true, required: true },
+   },
+
+   /**
+    * fn
+    * our Request handler.
+    * @param {obj} req
+    *        the request object sent by the
+    *        api_sails/api/controllers/definition_manager/json-import.
+    * @param {fn} cb
+    *        a node style callback(err, results) to send data when job is finished
+    */
+   fn: function handler(req, cb) {
+      req.log("definition_manager.json-reset:");
+
+      req.servicePublish("definition.stale", {});
+      cb();
+   },
+};

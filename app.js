99d84cf07845ea8e6b739713287cd48b577764d3
nohrsom <sky_wyvern@hotmail.com>
2023-03-31 13:06:21 +0700

    Fix ab-utils name

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // definition_manager
 // (AppBuilder) A service to manage the definitions for a running AppBuilder platform.
 //
-const AB = require("ab-utils");
+const AB = require("digiserve/ab-utils");
 
 var controller = AB.controller("definition_manager");
 controller.afterStartup((req, cb) => {

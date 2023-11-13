50cafdbb5115d684cc491be02e2f96db7a5b9614
nohrsom <sky_wyvern@hotmail.com>
2023-11-13 12:05:38 +0700

    Refactor to async function

--- handlers/definitionsForRoles.js
+++ handlers/definitionsForRoles.js
@@ -29,96 +29,100 @@ module.exports = {
     */
    fn: async function handler(req, cb) {
       //
-      var ServiceKey = this.key;
+      const ServiceKey = this.key;
+
       req.log(ServiceKey);
 
       // Get the passed in parameters
-      var roles = req.param("roles");
-      let roleIDs = roles.map((r) => r.uuid);
+      const roles = req.param("roles");
+      const roleIDs = roles.map((r) => r.uuid);
+
       roleIDs.sort(); // so they will always be in same order.
 
       req.log("-- roles: --", roleIDs);
 
-      ABBootstrap.init(req)
-         .then(async (AB) => {
-            let hashIDs = AB.cache("defs-for-role");
-            if (!hashIDs) hashIDs = {};
+      try {
+         const AB = await ABBootstrap.init(req);
 
-            const isSystemUser =
-               roleIDs.filter((roleId) =>
-                  AB.defaultSystemRoles().includes(roleId)
-               ).length > 0;
+         let hashIDs = AB.cache("defs-for-role");
+         if (!hashIDs) hashIDs = {};
 
-            var ids = [];
-            // {array}
-            // all the ABDefinition.id that need to be exported.
+         let ids = [];
+         // {array}
+         // all the ABDefinition.id that need to be exported.
 
-            var hashKey = roleIDs.join(",");
-            if (!hashIDs[hashKey] || hashIDs[hashKey].length == 0) {
-               req.log("building ID hash");
-               req.performance.mark("buildIDHash");
-               var applications = AB.applications(
-                  (a) => isSystemUser || a.isAccessibleForRoles(roles)
-               );
+         const hashKey = roleIDs.join(",");
 
-               req.log(
-                  `definition_manager.definitionsForRoles: found ${applications.length} applications to export`
-               );
+         if (!hashIDs[hashKey] || hashIDs[hashKey].length == 0) {
+            req.log("building ID hash");
+            req.performance.mark("buildIDHash");
 
-               // This takes a long time!
-               // Cache this?
-               let aIDs = [];
-               applications.forEach((a) => {
-                  a.exportIDs(aIDs);
-               });
-
-               // NOTE: we also need to make sure all the System Objects in the definitions.
-               let systemObjects = AB.objects((o) => o.isSystemObject);
-               for (var i = 0; i < systemObjects.length; i++) {
-                  systemObjects[i].exportIDs(aIDs);
-               }
-
-               hashIDs[hashKey] = aIDs;
-
-               AB.cache("defs-for-role", hashIDs);
-               req.performance.measure("buildIDHash");
-            }
-
-            ids = hashIDs[hashKey];
-            req.log(
-               `definition_manager.definitionsForRoles: found ${ids.length} ids to export.`
+            const applications = AB.applications(
+               (a) =>
+                  // Check for a system user.
+                  roleIDs.filter((roleId) =>
+                     AB.defaultSystemRoles().includes(roleId)
+                  ).length > 0 || a.isAccessibleForRoles(roles)
             );
 
-            const stringifiedDefs = AB.cache("cached-defs") || {};
+            req.log(
+               `definition_manager.definitionsForRoles: found ${applications.length} applications to export`
+            );
 
-            if (stringifiedDefs[hashKey] == null) {
-               req.performance.mark("stringify-defs-for-role", {
-                  op: "serialize",
-               });
+            // This takes a long time!
+            // Cache this?
+            const aIDs = [];
 
-               stringifiedDefs[hashKey] = await req.worker(
-                  (defs) => JSON.stringify(defs),
-                  [
-                     ids
-                        .map((id) => AB.definitionByID(id, true))
-                        .filter((def) => def != null),
-                  ]
-               );
-
-               req.performance.measure("stringify-defs-for-role");
-               AB.cache("cached-defs", stringifiedDefs);
-            }
-
-            cb(null, stringifiedDefs[hashKey]);
-         })
-         .catch((err) => {
-            // we clear the cache just in case our data was incorrect.
-            // AB.cacheClear(ServiceKey);
-            req.notify.developer(err, {
-               context:
-                  "Service:definition_manager.definitionsForRoles: Error initializing ABFactory",
+            applications.forEach((a) => {
+               a.exportIDs(aIDs);
             });
-            cb(err);
+
+            // NOTE: we also need to make sure all the System Objects in the definitions.
+            AB.objects((o) => o.isSystemObject).forEach((systemObject) => {
+               systemObject.exportIDs(aIDs);
+            });
+
+            hashIDs[hashKey] = aIDs;
+
+            AB.cache("defs-for-role", hashIDs);
+            req.performance.measure("buildIDHash");
+         }
+
+         ids = hashIDs[hashKey];
+
+         req.log(
+            `definition_manager.definitionsForRoles: found ${ids.length} ids to export.`
+         );
+
+         const stringifiedDefs = AB.cache("cached-defs") || {};
+
+         if (stringifiedDefs[hashKey] == null) {
+            req.performance.mark("stringify-defs-for-role", {
+               op: "serialize",
+            });
+
+            stringifiedDefs[hashKey] = await req.worker(
+               (defs) => JSON.stringify(defs),
+               [
+                  ids
+                     .map((id) => AB.definitionByID(id, true))
+                     .filter((def) => def != null),
+               ]
+            );
+
+            req.performance.measure("stringify-defs-for-role");
+            AB.cache("cached-defs", stringifiedDefs);
+         }
+
+         cb(null, stringifiedDefs[hashKey]);
+      } catch (error) {
+         // we clear the cache just in case our data was incorrect.
+         // AB.cacheClear(ServiceKey);
+         req.notify.developer(error, {
+            context:
+               "Service:definition_manager.definitionsForRoles: Error initializing ABFactory",
          });
+         cb(error);
+      }
    },
 };
